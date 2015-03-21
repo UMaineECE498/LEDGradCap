c5ba9dd2f4586d379905fb848e7533b3ff704580
Sheaff <andysheaff@gmail.com>
2015-03-21 19:33:14 -0400

    Add led.h - header file for leds

---
+++ software/led.h
@@ -0,0 +1,13 @@
+// A. Sheaff 1/24/2015
+// ECE 498 Grad Cap led header
+#ifndef LED_H
+#define LED_H
+#include <stdint.h>
+
+// Send 8, 24 bit "frames" to 8 leds.
+// ra contains ! 8 ! red values
+// rb contains ! 8 ! green values
+// rc contains ! 8 ! blue values
+void send_leds(uint8_t *ra, uint8_t *ga, uint8_t *ba);
+
+#endif // LED_H
