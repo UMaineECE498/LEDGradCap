def96695a77d4a1b159dcc542bf6877678789cd8
Blake <techplex.engineer@gmail.com>
2015-05-04 12:06:01 -0400

    More colors!

--- software/ledRoutines.c
+++ software/ledRoutines.c
@@ -8,16 +8,13 @@
 
 static uint8_t i, r, g, b;
 
-void fadecolor(int8_t rinc, int8_t ginc, int8_t binc, int8_t rstep, int8_t gstep, int8_t bstep)
+void fadecolor(int8_t rinc, int8_t ginc, int8_t binc)
 {
-	for (i=0;i<64;i++)
+	for (i=1;i<64;i++)
 	{
-		if (i&rstep)
-			r += rinc;
-		if (i&gstep)
-			g += ginc;
-		if (i&bstep)
-			b += binc;
+		r += rinc;
+		g += ginc;
+		b += binc;
 
 		memset(red,r,8);
 		memset(green,g,8);
@@ -31,56 +28,53 @@ void myfade(void)
 {
 
 
-	r=64;
-	g=64;
-	b=64;
-	fadecolor(-1, -1, -1, 0x01, 0x01, 0x01);
-	// for (i=0;i<64;i++) {
-	// 	if (i&0x01) {
-	// 		r--;
-	// 		g--;
-	// 		b--;
-	// 	}
-	// 	memset(red,r,8);
-	// 	memset(green,g,8);
-	// 	memset(blue,b,8);
-	// 	send_leds(red,green,blue);
-	// 	_delay_ms(FADE_DELAY);
-	// }
-	fadecolor(1, 1, 1, 0x01, 0x01, 0x01);
-	// for (i=0;i<64;i++) {
-	// 	if (i&0x01) {
-	// 		r++;
-	// 		g++;
-	// 		b++;
-	// 	}
-	// 	memset(red,r,8);
-	// 	memset(green,g,8);
-	// 	memset(blue,b,8);
-	// 	send_leds(red,green,blue);
-	// 	_delay_ms(FADE_DELAY);
-	// }
-	
-	fadecolor(-1, -1, 0);
-	// for (i=0;i<64;i++) {
-	// 	r--;
-	// 	g--;
-	// 	memset(red,r,8);
-	// 	memset(green,g,8);
-	// 	memset(blue,b,8);
-	// 	send_leds(red,green,blue);
-	// 	_delay_ms(FADE_DELAY);
-	// }
-	fadecolor(1, 1, 0);
-	// for (i=0;i<64;i++) {
-	// 	r++;
-	// 	g++;
-	// 	memset(red,r,8);
-	// 	memset(green,g,8);
-	// 	memset(blue,b,8);
-	// 	send_leds(red,green,blue);
-	// 	_delay_ms(FADE_DELAY);
-	// }
+	r=0;
+	g=0;
+	b=0;
+	//fade from off to full white
+	fadecolor(1, 1, 1);
+
+	//fade from white to G+B
+	fadecolor(0, -1, 0);
+
+	//fade from G+B => B
+	fadecolor(-1, 0, 0);
+
+	//fade from B => B+R
+	fadecolor(0, 1, 0);
+
+	//fade from B+R => R+G+B (White)
+	fadecolor(1, 0, 0);
+
+	//fade from white to R+G
+	fadecolor(0, 0, -1);
+
+	//fade from R+G => R
+	fadecolor(0, -1, 0);
+
+	//fade from R => R+B
+	fadecolor(0, 0, 1);
+
+	//fade from B+R => R+G+B (White)
+	fadecolor(0, 1, 0);
+
+	//fade from white to R+G
+	fadecolor(-1, 0, 0);
+
+	//fade from R+G => R
+	fadecolor(0, 0, -1);
+
+	//fade from R => R+B
+	fadecolor(1, 0, 0);
+
+	//fade from B+R => R+G+B (White)
+	fadecolor(0, 0, 1);
+
+	//fade from white to off
+	fadecolor(-1, -1, -1);
+
+
+
 }
 
 
